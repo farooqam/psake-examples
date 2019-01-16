e627762dd08df517e6f0c3097201f167327bc017
farooqam <farooq_am@hotmail.com>
2019-01-15 16:40:47 -0800

    Initial commit

---
+++ NetCoreConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NetCoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press ENTER to quit.");
+            Console.ReadLine();
+        }
+    }
+}
